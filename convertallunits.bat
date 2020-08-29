994ac25afd7b41ed14ed668766b8a9587cb16272
Axle1975 <me@here.com>
2020-08-29 16:25:21 +1000

    add batch file harness for convertallunits.py

---
+++ convertallunits.bat
@@ -0,0 +1,1 @@
+python convertallunits.py --input-spec d:\temp\ccdata\UNITS\*.fbi --converter-path d:\temp\3do2scm\3do2scm.exe --exporter-path d:\temp\3do2scm\supcom-exporter.py --objects3d-paths d:\temp\totala1\objects3d\ d:\temp\ccdata\objects3d\
