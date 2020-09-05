9b4ac2a197a260b769de271e61a431c0c4b1acbd
Axle1975 <me@here.com>
2020-09-05 20:23:50 +1000

    quote the command line arguments incase use substitutes paths with spaces in them

--- convertallunits.bat
+++ convertallunits.bat
@@ -1,2 +1,2 @@
-python convertallunits.py --input-spec d:\temp\ccdata\UNITS\*.fbi --converter-path d:\temp\3do2scm\3do2scm.exe --exporter-path d:\temp\3do2scm\supcom-exporter.py --tadata-paths d:\temp\ccdata d:\temp\totala1
+convertallunits.exe --input-spec "d:\temp\ccdata\UNITS\*.fbi" --tadata-paths "d:\temp\ccdata" "d:\temp\totala1"
 pause
