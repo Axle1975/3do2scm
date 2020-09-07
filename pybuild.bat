02af13ec7126ea99c103aadf35b292c502c12dea
Axle1975 <me@here.com>
2020-09-07 19:26:57 +1000

    documentation

--- pybuild.bat
+++ pybuild.bat
@@ -2,5 +2,6 @@ rem This script packages the python side of the app into an exe.
 rem You still need to build the c++ side using cmake
 rem
 rem NB you need pyinstaller (a python util) on your path. eg by activating a virtualenv.
-pyinstaller -F convertallunits.py
+pyinstaller -F -p scm convertallunits.py
+pyinstaller -F nbos2sca.py
 pause
