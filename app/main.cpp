fa0b67b1c3d462932f121bf9a4d92d9b8576ad05
Axle1975 <me@here.com>
2020-09-11 11:21:32 +1000

    documentation; and remove redundant rgb2bgr script

--- app/main.cpp
+++ app/main.cpp
@@ -622,7 +622,6 @@ int main(int argc, char **argv)
             {
                 textures->saveTextures(albedo);
                 textures->saveLogos(specteam);
-
             }
 
             std::cout << "{" << JsonKey("root");
