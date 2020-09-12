2ababc17a11266e2e30f4b0863e8ef09dd116445
Axle1975 <me@here.com>
2020-09-12 19:20:32 +1000

    add some features to allow flexibilty with coordinate frames

--- nbos2sca.py
+++ nbos2sca.py
@@ -1,5 +1,6 @@
 import copy
 import numpy
+import os
 import struct
 import sys
 
@@ -10,41 +11,68 @@ class Piece:
         @param name, parent: strings
         """
 
+        self.xyz0 = copy.copy(list(xyz0))
+        self.rpw0 = copy.copy(list(rpw0))
+
+        self.xyz_offset = [0., 0., 0.]
+        self.rpw_offset = [0., 0., 0.]
+
         self.name = name
         self.parent = parent
 
-        self.cur_xyz = [ [x for x in xyz0] ]
+        self.cur_xyz = [ copy.copy(self.xyz0) ]
         self.vel_xyz = [0., 0., 0.]
-        self.target_xyz = [x for x in xyz0]
+        self.target_xyz = copy.copy(self.xyz0)
 
         # rpw: roll(about +X), pitch(about +Y), yaw(about +Z)
-        self.cur_rpw = [ [x for x in rpw0] ]
+        self.cur_rpw = [ copy.copy(self.rpw0) ]
         self.rate_rpw = [0., 0., 0.]
-        self.target_rpw = [x for x in rpw0]
+        self.target_rpw = copy.copy(self.rpw0)
+
+    def reset(self):
+        self.cur_xyz = [ self.cur_xyz[-1] ]
+        self.vel_xyz = [0., 0., 0.]
+        self.target_xyz = copy.copy(self.cur_xyz[-1])
+
+        # rpw: roll(about +X), pitch(about +Y), yaw(about +Z)
+        self.cur_rpw = [ self.cur_rpw[-1] ]
+        self.rate_rpw = [0., 0., 0.]
+        self.target_rpw = copy.copy(self.cur_rpw[-1])
 
     def get_frames(self):
         return self.cur_xyz, self.cur_rpw
 
+    def set_move_offset(self, axis_idx, x0):
+        self.xyz_offset[axis_idx] = x0
+
+    def set_turn_offset(self, axis_idx, x0):
+        self.rpw_offset[axis_idx] = x0
+
     def move_now(self, axis_idx, target):
-        self.cur_xyz[-1][axis_idx] = self.cur_xyz[0][axis_idx] + target
+        target +=  self.xyz_offset[axis_idx]
+        self.cur_xyz[-1][axis_idx] = self.xyz0[axis_idx] + target
         self.vel_xyz[axis_idx] = 0.
         self.target_xyz[axis_idx] = self.cur_xyz[-1][axis_idx]
 
     def turn_now(self, axis_idx, target):
-        self.cur_rpw[-1][axis_idx] = self.cur_rpw[0][axis_idx] + target
+        target +=  self.rpw_offset[axis_idx]
+        self.cur_rpw[-1][axis_idx] = self.rpw0[axis_idx] + target
         self.rate_rpw[axis_idx] = 0.
         self.target_rpw[axis_idx] = self.cur_rpw[-1][axis_idx]
 
     def move_at_speed(self, axis_idx, target, speed):
+        target +=  self.xyz_offset[axis_idx]
         self.vel_xyz[axis_idx] = speed
-        self.target_xyz[axis_idx] = self.cur_xyz[0][axis_idx] + target
+        self.target_xyz[axis_idx] = self.xyz0[axis_idx] + target
 
     def turn_at_speed(self, axis_idx, target, speed):
+        target +=  self.rpw_offset[axis_idx]
         self.rate_rpw[axis_idx] = speed
-        self.target_rpw[axis_idx] = self.cur_rpw[0][axis_idx] + target
+        self.target_rpw[axis_idx] = self.rpw0[axis_idx] + target
 
     def step(self, dt):
         # dt in seconds
+
         def update(x, speed, xtarget, dt):
             # return new_x, new_speed
             if (xtarget-x)*speed >= 0.:
@@ -73,7 +101,7 @@ def parse_nbos(script):
         _statement = _statement.strip()
         statement = _statement.replace(',', ' ')
         statement = statement.replace('\n', ' ')
-        words = [w.lower() for w in statement.split(' ') if len(w)>0]
+        words = [w for w in statement.split(' ') if len(w)>0]
         if len(words) == 0:
             continue
         statements += [words]
@@ -82,55 +110,88 @@ def parse_nbos(script):
     return statements, vars
 
 
-def run_nbos(statements, pieces, fps):
+def run_nbos(statements, pieces, vars, fps):
     """
     @param script: string containing the (not)bos script
     @param pieces: dictionary of Piece objects
     """
 
     SCALE_FACTORS = [2.5, 2.5, -2.5]
-    def apply_scale(coord, axis):
+    def apply_scale_factor(coord, axis):
         return coord/SCALE_FACTORS[axis]
+        
+    ROTATION_SCALE_FACTORS = [1.0, -1.0, 1.0]
+    def apply_rotation_factor(coord, axis):
+        return coord*numpy.sign(ROTATION_SCALE_FACTORS[axis])
 
     def str_to_axis_idx(axis_str):
         return { 'x-axis':0, 'y-axis':1, 'z-axis':2 }[axis_str]
 
+    t = 0.
     def to_float(number_str):
         try:
             return float(number_str)
         except ValueError:
-            return float(number_str[1:-1])
+            if number_str[0]=="'" and number_str[-1]=="'":
+                return eval(number_str[1:-1], {'t':t, 'cos':numpy.cos, 'sin':numpy.sin})
+            else:
+                return float(number_str[1:-1])
 
     for words in statements:
+        words[0] = words[0].lower()
 
         if words[0] == 'scales':
             SCALE_FACTORS = [ to_float(w) for w in (words[1],words[2],words[3]) ]
 
+        elif words[0] == 'rotation-scales':
+            ROTATION_SCALE_FACTORS = [ to_float(w) for w in (words[1],words[2],words[3]) ]
+
         elif words[0] == 'scm-file-path':
             pass
+            
+        elif words[0] == 'not-looped':
+            pass
+
+        elif words[0] == 'set-turn-offset':
+            name, axis, position = words[1], str_to_axis_idx(words[3]), to_float(words[4])
+            pieces[name].set_turn_offset(axis, position)
+            
+        elif words[0] == 'set-move-offset':
+            name, axis, position = words[1], str_to_axis_idx(words[3]), to_float(words[4])
+            pieces[name].set_move_offset(axis, position)
 
         elif words[0] == 'move':
-            name, axis, position, speed = words[1], str_to_axis_idx(words[3]), to_float(words[4]), words[5]
+            name, axis, position, speed = words[1], str_to_axis_idx(words[3]), to_float(words[4]), words[5].lower()
             if speed=='now':
-                pieces[name].move_now(axis, apply_scale(position, axis))
+                if t>0. or 'not-looped' in vars:
+                    pieces[name].move_now(axis, apply_scale_factor(position, axis))
             elif speed=='speed':
                 speed = to_float(words[6])
-                pieces[name].move_at_speed(axis, apply_scale(position,axis), apply_scale(speed,axis))
+                pieces[name].move_at_speed(
+                    axis,
+                    apply_scale_factor(position,axis),
+                    apply_scale_factor(speed,axis))
 
         elif words[0] == 'turn':
-            name, axis, position, speed = words[1], str_to_axis_idx(words[3]), to_float(words[4]), words[5]
+            name, axis, position, speed = words[1], str_to_axis_idx(words[3]), to_float(words[4]), words[5].lower()
             if speed=='now':
-                pieces[name].turn_now(axis, position)
+                if t>0. or 'not-looped' in vars:
+                    pieces[name].turn_now(axis, apply_rotation_factor(position, axis))
             elif speed=='speed':
-                pieces[name].turn_at_speed(axis, position, to_float(words[6]))
+                pieces[name].turn_at_speed(
+                    axis,
+                    apply_rotation_factor(position, axis),
+                    apply_rotation_factor(to_float(words[6]), axis))
 
         elif words[0] == 'sleep':
             dt = 1. / fps
             for name,piece in pieces.items():
-                t = to_float(words[1]) / 1000.
-                while t > dt-1e-3:
+                countdown = to_float(words[1]) / 1000.
+                countdown = max(dt, countdown)
+                while countdown > dt-1e-3:
                     piece.step(dt)
-                    t -= dt
+                    countdown -= dt
+            t += to_float(words[1]) / 1000.
 
         else:
             raise ValueError("unknown nBOS command: '{}'".format(words[0]))
@@ -253,16 +314,10 @@ def to_sca(pieces, fps):
             pos_xyz_per_bone_per_frame[frame_num,bone_num,:] = xyz
         for frame_num,rpw in enumerate(rpw_per_frame):
             bone_num = bone_names.index(name)
-            orientation_wxyz_per_bone_per_frame[frame_num,bone_num,:] = rpw_to_quaternion(rpw)
+            q = rpw_to_quaternion(rpw)
+            orientation_wxyz_per_bone_per_frame[frame_num,bone_num,:] = q
 
     # root bone delta
-    """
-    root_bone_idx = bone_links.index(-1)
-    root_pos_delta = pos_xyz_per_bone_per_frame[-1,root_bone_idx,:] - pos_xyz_per_bone_per_frame[0,root_bone_idx,:]
-    root_orientation_delta = unit_quaternion_divide(
-        orientation_wxyz_per_bone_per_frame[-1,root_bone_idx,:],
-        orientation_wxyz_per_bone_per_frame[0,root_bone_idx,:])
-    """
     root_pos_delta = [0., 0., 0.]
     root_orientation_delta = [1., 0., 0., 0.]
 
@@ -292,6 +347,52 @@ def construct_pieces(scm_filename):
     return pieces
 
 
+def process_nbos(nbosfile, args):
+    print("NBOS script:{}".format(nbosfile))
+    with open(nbosfile, 'rt') as file:
+        script = file.read()
+    statements, vars = parse_nbos(script)
+
+    try:
+        # command line scm overrides scm-file-path directive in nbos file
+        scmfile = args.scmfile or vars['scm-file-path']
+    except KeyError:
+        # otherwise default look for scm with similar name/path as nbos file
+        scmfile = os.path.splitext(nbosfile)[0]+'.scm'
+
+    scmdir = os.path.dirname(scmfile)
+    nbosdir = os.path.dirname(nbosfile)
+    if scmdir=='' and nbosdir!='':
+        # prepend a path if required and available
+        scmfile = os.path.join(nbosdir,scmfile)
+
+    print("  SCM input:{}".format(scmfile))
+    pieces = construct_pieces(scmfile)
+    run_nbos(statements, pieces, vars, args.fps)
+    
+    if not 'not-looped' in vars:
+        # run again using final position as new starting position
+        for _,piece in pieces.items():
+            piece.reset()
+        run_nbos(statements, pieces, vars, args.fps)
+
+    scafile = args.scafile or os.path.splitext(nbosfile)[0]+'.sca'
+    print("  SCA output:{}".format(scafile))
+    with open(scafile, 'wb') as file:
+        file.write(to_sca(pieces, args.fps))
+
+
+def recursive_process_filespec(filespec, args):
+
+    if os.path.isfile(filespec) and os.path.splitext(filespec)[-1].lower()==".nbos":
+        process_nbos(filespec, args)
+        
+    elif os.path.isdir(filespec):
+        for subdir, dirs, files in os.walk(filespec):
+            for file in files:
+                recursive_process_filespec(os.path.join(subdir,file), args)
+
+
 if __name__ == "__main__":
 
     try:
@@ -301,39 +402,14 @@ if __name__ == "__main__":
         import traceback
 
         parser = argparse.ArgumentParser()
-        parser.add_argument('nbosfiles', nargs='*', help='path to the .nbos (not)bos file(s) containing the TA annimation script')
+        parser.add_argument('filespec', nargs='*', help='path to the .nbos (not)bos file(s) containing the TA annimation script, and/or directories in which to search for .nbos files.')
         parser.add_argument('--scmfile', help='path to the existing file containing .scm supcom model associated with the script. Overrides any "scm-file-path" statement in the nBOS file', default=None)
         parser.add_argument('--scafile', help='path of the new supcom .sca animation file to create.  Default matches the nbosfile but with extension ".sca"', default=None)
         parser.add_argument('--fps', type=float, help='frames per second.  default=30', default=30.)
         args = parser.parse_args()
 
-        for nbosfile in args.nbosfiles:
-            print("NBOS script:{}".format(nbosfile))
-            with open(nbosfile, 'rt') as file:
-                script = file.read()
-            statements, vars = parse_nbos(script)
-
-            try:
-                # command line scm overrides scm-file-path directive in nbos file
-                scmfile = args.scmfile or vars['scm-file-path']
-            except KeyError:
-                # otherwise default look for scm with similar name/path as nbos file
-                scmfile = os.path.splitext(nbosfile)[0]+'.scm'
-
-            scmdir = os.path.dirname(scmfile)
-            nbosdir = os.path.dirname(nbosfile)
-            if scmdir=='' and nbosdir!='':
-                # prepend a path if required and available
-                scmfile = os.path.join(nbosdir,scmfile)
-
-            print("  SCM input:{}".format(scmfile))
-            pieces = construct_pieces(scmfile)
-            run_nbos(statements, pieces, args.fps)
-
-            scafile = args.scafile or os.path.splitext(nbosfile)[0]+'.sca'
-            print("  SCA output:{}".format(scafile))
-            with open(scafile, 'wb') as file:
-                file.write(to_sca(pieces, args.fps))
+        for filespec in args.filespec:
+            recursive_process_filespec(filespec, args)
 
     except:
         traceback.print_exc()
